b4eae9088988527b90a3b21432ba412cf14c9cc6
ariel <avranico@gmail.com>
2025-03-30 00:01:15 +0300

    flat array

--- create flat array/myScript.js
+++ create flat array/myScript.js
@@ -1,15 +1,8 @@
-
-
-
-const arr1 = [1, 2, 3, 1, 4];
-const arr2 = [[1, 2, 3, [1, 2, 3, 1, 4], 1, 4], 1, 2, 3, 4];
-const arr3 = [[0], 1, 2, 3, 1, 4];
-
-function flatArray(myArr) {
+function flatArray1(myArr) {
     let result = []
     for (let i = 0; i < myArr.length; i++) {
         if (myArr[i].length) {
-            result.push(...flatArray(myArr[i]));
+            result.push(...flatArray1(myArr[i]));
         } else {
             result.push(myArr[i])
         }
@@ -17,7 +10,16 @@ function flatArray(myArr) {
     return result;
 }
 
+function flatArray2(myArr) {
+    return myArr.flat(Infinity);
+}
 
-const result = flatArray(arr2);
-console.log('result',result);
+const arr1 = [1, 2, 3, 1, 4];
+const arr2 = [[1, 2, 3, [1, 2, 3, 1, 4], 1, 4], 1, 2, 3, 4];
+const arr3 = [[0], 1, 2, 3, 1, 4];
+
+
+const result = flatArray2(arr2);
+
+console.log('result', result);
 
