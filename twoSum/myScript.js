effd7c66b7f21ce216ae3060c6ef35d534c77460
ariel <avranico@gmail.com>
2025-03-31 19:24:19 +0300

    adding folder

--- twoSum/myScript.js
+++ twoSum/myScript.js
@@ -1,3 +1,4 @@
-//function get array and 1 value
+//function get array and 1 number value
 //should return the index of the values that together equale to the 1 value that got as second argument
- 
+//for example:
+//[1,2,3] and the number is 3 - > the function should return [0,1]
