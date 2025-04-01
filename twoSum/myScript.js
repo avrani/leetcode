6909ff1e7aa97afcc8311008ff5f093d1450bd79
ariel <avranico@gmail.com>
2025-04-01 21:12:24 +0300

    another solution but I dont understand the logic

--- twoSum/myScript.js
+++ twoSum/myScript.js
@@ -1,4 +1,32 @@
 //function get array and 1 number value
-//should return the index of the values that together equale to the 1 value that got as second argument
+//should return the index of the 2 values that together equale to the 1 value that got as second argument
 //for example:
 //[1,2,3] and the number is 3 - > the function should return [0,1]
+
+
+const twoSum1 = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] + arr[j] === value && i != j) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+
+const twoSum2 = (nums, target) => {
+    let numMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+        numMap.set(nums[i], i);
+    }
+    return [];
+}
+
+
+const result2 = twoSum2([4, 2, 5, 7, 1, 3], 9);
+console.log('result2', result2);
